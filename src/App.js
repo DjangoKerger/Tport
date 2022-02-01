35ffb0cf0de9495b9f1b2ee42fe2ed3f0f346b15
“First <Kerger06@aol.com>
2022-02-01 14:52:50 -0600

    ran into dead end

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.scss';
 import Header from "./Header";
 import Body from "./Body";
+import Menu from './Menu';
 
 function App() {
+
   return (
     <>
+    <Menu />
     <body id="bg-img">
     <div class="overlay"></div>
     <Header />
