b746ae2d5d6669a362a7c9db937f5bc5c3431021
“First <Kerger06@aol.com>
2022-02-01 13:59:59 -0600

    Added background image and styled

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
-import Header from "./Header"
+import './App.scss';
+import Header from "./Header";
+import Body from "./Body";
 
 function App() {
   return (
     <>
+    <body id="bg-img">
+    <div class="overlay"></div>
     <Header />
-
+    <Body />
+    </body>
     </>
   );
 }
